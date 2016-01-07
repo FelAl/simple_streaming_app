d15b102ade5f44595a094a4292159bec13a55e69
Fedorov Alexander I <alexander.fedorov@rtlabs.ru>
2016-01-07 16:24:38 +0300

    rdd test

--- src/main/scala/com/test/core/streaming/main.scala
+++ src/main/scala/com/test/core/streaming/main.scala
@@ -1,7 +1,5 @@
 import org.apache.spark._
 import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.log4j.Logger
@@ -17,11 +15,16 @@ object SimpleApp {
     }})
     val Array(zkQuorum, group, topics, numThreads) = args
     val sparkConf = new SparkConf().setMaster("local[2]").setAppName("SimpleApp")
-    val ssc = new StreamingContext(sparkConf, Seconds(1))
+    val ssc = new StreamingContext(sparkConf, Seconds(4))
     val topicMap = topics.split(",").map((_, 1)).toMap
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
-    val words = lines.flatMap(_.split(" "))
-    words.print()
+    lines.foreachRDD { rdd =>
+      val words = rdd.flatMap(_.split(" "))
+      Log.info("WORDS WORDS:" + words)
+    }
+
+    //val words = lines.flatMap(_.split(" "))
+    // words.print()
     Log.info("DEBUG info:" + zkQuorum)
     
     ssc.start()
