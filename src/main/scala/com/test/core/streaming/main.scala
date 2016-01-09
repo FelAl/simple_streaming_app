be69c1d8072d3834036dd2622d5210ab97ce08c4
Fedorov Alexander I <alexander.fedorov@rtlabs.ru>
2016-01-10 01:50:16 +0300

    hook + foreachRDD

--- src/main/scala/com/test/core/streaming/main.scala
+++ src/main/scala/com/test/core/streaming/main.scala
@@ -8,25 +8,28 @@ object SimpleApp {
   val Log = Logger.getLogger(SimpleApp.this.getClass().getSimpleName())
 
   def main(args: Array[String]) {
-    val mainThread = Thread.currentThread();
-    Runtime.getRuntime.addShutdownHook(new Thread() {override def run = {
-      Log.info("----CTRL-C HANDLING-----")
-      mainThread.join()
-    }})
+    // val mainThread = Thread.currentThread();
     val Array(zkQuorum, group, topics, numThreads) = args
     val sparkConf = new SparkConf().setMaster("local[2]").setAppName("SimpleApp")
-    val ssc = new StreamingContext(sparkConf, Seconds(4))
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
     val topicMap = topics.split(",").map((_, 1)).toMap
-    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
-    lines.foreachRDD { rdd =>
-      val words = rdd.flatMap(_.split(" "))
-      Log.info("WORDS WORDS:" + words)
-    }
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).foreachRDD( rdd => 
+      if(rdd.isEmpty() == false){
+        rdd.foreach(x => println(x._2))
+      }
+    )
 
-    //val words = lines.flatMap(_.split(" "))
-    // words.print()
-    Log.info("DEBUG info:" + zkQuorum)
-    
+    Log.warn("DEBUG info:" + zkQuorum)
+    sys.addShutdownHook(() => {
+      println("< received SIGTERM (shutdown hook)")
+      Log.warn("----CTRL-C HANDLING-----")
+      Log.error("----CTRL-C HANDLING----- Log error")
+      // val sc = SparkContext.getOrCreate()
+      try ssc.stop(true) catch { case _ : Throwable => { } }
+      // sc.stop()
+      sys.exit(0)
+    })
+
     ssc.start()
     ssc.awaitTermination()
   }
